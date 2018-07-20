d825327f7e2b46e2cafd0f3dbf5ac9d871d3c4d6
Evgeny Danienko <6655321@bk.ru>
2018-07-20 15:25:30 +0300

    week3 token

--- week_03/bonusToken/contracts/BonusToken.sol
+++ week_03/bonusToken/contracts/BonusToken.sol
@@ -6,32 +6,74 @@ import "zeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 contract BonusToken is MintableToken, DetailedERC20 {
 
     constructor(uint prefund) public
-        DetailedERC20("my test token", "MTT", 18)
+    DetailedERC20("CustomerBonusToken", "CBT", 18)
     {
         balances[msg.sender] = prefund;
         totalSupply_ = balances[msg.sender];
         emit Transfer(address(0), msg.sender, totalSupply_);
     }
 
-    modifier notFrozen(address owner) {
-        require(freezedFounds[owner] < now);
-        _;
-    }
+    event NewOrder(address indexed owner, uint id, uint256 amount, uint toDate);
+    event NewXXX(address indexed owner, order o, bool res);
 
-    function freeze(uint toDate) public {
-        require(toDate > now);
-        require(freezedFounds[msg.sender] < toDate);
-
-        freezedFounds[msg.sender] = toDate;
-    }
-
-    function transfer(address _to, uint256 _value) public notFrozen(msg.sender) returns (bool) {
+    function transfer(address _to, uint256 _value) public canPay(msg.sender, _value) returns (bool) {
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public notFrozen(_from) returns (bool) {
+    function transferFrom(address _from, address _to, uint256 _value) public canPay(_from, _value) returns (bool) {
         return super.transferFrom(_from, _to, _value);
     }
 
-    mapping (address => uint) public freezedFounds;
+    function makeOrder(uint256 _price, uint _toDate) public canPay(msg.sender, _price) returns (bool) {
+        require(_toDate > now);
+
+        uint _orderID = orders.push(order(false, msg.sender, _toDate, _price))-1;
+        balancesFrozen[msg.sender] += _price;
+
+        emit NewOrder(msg.sender, _orderID, _price, _toDate);
+
+        return true;
+    }
+
+    function orderComplete(uint _orderID) public returns (bool) {
+        // check is owner
+        order storage userOrder = orders[_orderID];
+        require(!userOrder.complete);
+
+        // check signatures of user and seller
+        // check is the order belongs to the user
+
+        userOrder.complete = true;
+        balancesFrozen[msg.sender] -= userOrder.price;
+
+        //bonus
+        //todo: make `1 hours` constant changeable by the contract owner
+        if ((userOrder.toDate+1 hours) >= now) {
+            //todo: make bonus percent constant changeable by the contract owner
+            transfer(userOrder.owner, userOrder.price/10);
+        } else {
+            transferFrom(msg.sender, owner, userOrder.price/10);
+        }
+
+        return true;
+    }
+
+    function availableBalanceOf(address who) public view returns (uint256) {
+        return balanceOf(who)-balancesFrozen[who];
+    }
+
+    modifier canPay(address owner, uint256 _price) {
+        require((balanceOf(msg.sender)-balancesFrozen[msg.sender]) >= _price);
+        _;
+    }
+
+    struct order {
+        bool complete;
+        address owner;
+        uint toDate;
+        uint256 price;
+    }
+    order[] orders;
+
+    mapping(address => uint256) balancesFrozen;
 }
--- week_03/bonusToken/test/BonusTokenTest.js
+++ week_03/bonusToken/test/BonusTokenTest.js
@@ -17,15 +17,15 @@ contract('BonusTokenTest', function(accounts) {
         assertBigNumberEqual(await token.totalSupply(), wei(1000));
     });
 
-    it('test can transfer not freezed', async function() {
+    it('test can order', async function() {
         //given
         const token = await bonusToken.new(wei(initialAmount), {from: accounts[0]});
 
         //when
-        await token.transfer(accounts[1], wei(1), {from: accounts[0]});
+        await token.transfer(accounts[1], 1, {from: accounts[0]});
 
         //then
-        await token.transfer(accounts[1], wei(1), {from: accounts[0]});
+        await token.makeOrder(1, now()+60, {from: accounts[1]});
     });
 
     it('test cant transfer freezed', async function() {
@@ -34,12 +34,54 @@ contract('BonusTokenTest', function(accounts) {
 
         //when
         await token.transfer(accounts[1], 1, {from: accounts[0]});
-        await token.freeze(20000000000, {from: accounts[0]});
+        await token.makeOrder(1, now()+60, {from: accounts[1]});
 
         //then
-        await expectThrow(token.transfer(accounts[1], wei(1), {from: accounts[0]}));
+        await expectThrow(token.makeOrder(1, now()+60, {from: accounts[1]}));
     });
-    
+
+    it('test complete order', async function() {
+        //given
+        const token = await bonusToken.new(wei(initialAmount), {from: accounts[0]});
+
+        let eventHandler = token.NewOrder({fromBlock: 0, toBlock: 'latest'});
+        eventHandler.watch(async function(error, result) {
+            if (!error) {
+                //then
+                await token.orderComplete(result.args.id, {from: accounts[0]});
+                console.log("event", result.args, await token.balanceOf(accounts[1]))
+
+                assertBigNumberEqual(await token.balanceOf(accounts[1]), 11);
+            }
+        });
+
+        //when
+        await token.transfer(accounts[1], 10, {from: accounts[0]});
+        await token.makeOrder(10, now()+60, {from: accounts[1]});
+    });
+
+    it('test complete expired order', async function() {
+        //given
+        const token = await bonusToken.new(wei(initialAmount), {from: accounts[0]});
+
+        let eventHandler = token.NewOrder({fromBlock: 0, toBlock: 'latest'});
+        eventHandler.watch(function(error, result) {
+            if (!error) {
+                //then
+                setTimeout(async function() {
+                    console.log("event", result.args)
+                    await token.orderComplete(result.args.id, {from: accounts[0]});
+
+                    assertBigNumberEqual(await token.balanceOf(accounts[1]), 9);
+                }, 4000);
+            }
+        });
+
+        //when
+        await token.transfer(accounts[1], 10, {from: accounts[0]});
+        await token.makeOrder(10, now()+1, {from: accounts[1]});
+    });
+
     it('test contract owner can mint', async function() {
         //given
         const token = await bonusToken.new(wei(initialAmount), {from: accounts[0]});
@@ -70,4 +112,8 @@ contract('BonusTokenTest', function(accounts) {
 // converts amount of token into token-wei (smallest token units)
 function wei(amount) {
     return web3.toWei(amount, 'ether');
+}
+
+function now() {
+    return Math.floor(Date.now() / 1000);
 }
