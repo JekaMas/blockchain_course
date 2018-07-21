f2b2f31932fb180b3975ae4ea9bd66f701ac40d5
Evgeny Danienko <6655321@bk.ru>
2018-07-22 00:58:31 +0300

    week 3: green deploy

--- week_03/voting/migrations/2_deploy_token.js
+++ week_03/voting/migrations/2_deploy_token.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const voting = artifacts.require('Voting.sol');
+const startVoting = new Date();
+const startReveal = addDays(startVoting, 2);
+const terminationDate = addDays(startReveal, 2);
 
 module.exports = function(deployer, network) {
-    deployer.deploy(voting, startVoting, startReveal, terminationDate);
+    deployer.deploy(voting, startVoting.getTime(), startReveal.getTime(), terminationDate.getTime());
 };
+
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
